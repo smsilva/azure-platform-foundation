d3e4d2ad49f239d4e82138d092864107856d616f
Silvio Silva <smsilva@gmail.com>
2021-08-31 09:04:12 -0300

    wip

--- modules/environment/main.tf
+++ modules/environment/main.tf
@@ -9,3 +9,14 @@ resource "null_resource" "environment" {
     name = var.name
   }
 }
+
+resource "random_string" "environment_id" {
+  length      = 6
+  special     = false
+  upper       = false
+  min_numeric = 3
+}
+
+output "name" {
+  value = "${null_resource.environment.triggers.name}-${random_string.environment_id.result}"
+}
--- src/main.tf
+++ src/main.tf
@@ -1,9 +1,12 @@
 resource null_resource "foundation" {
-
 }
 
 module "environment" {
   source = "../modules/environment"
 
   name = "sandbox"
+}
+
+output "environment_name" {
+  value = module.environment.name
 }
